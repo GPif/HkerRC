81753f7a7273478c95d76fdc2b98b1d87866984f
GPif <gpifmail@yahoo.fr>
2015-03-16 23:14:09 +0100

    Add an interface for commands objects as a scaffold.

---
+++ app/src/main/java/com/gpif/hkerrc/com/gpif/hkerrc/cmds/Commands.java
@@ -0,0 +1,11 @@
+package com.gpif.hkerrc.com.gpif.hkerrc.cmds;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+
+public interface Commands {
+    String getName();
+    Activity getConfigActivity();
+    //String as parameters, Float between 0 and 1 as progress if any, Integer as retrun
+    AsyncTask<String,Float,Integer> getCmdAsyncTask();
+}
